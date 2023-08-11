a7c4b7d4e453703e49ebed5fc5b7e99c47f33abf
mostafa-m-desouky <mostafa.m.desouky@gmail.com>
2023-08-11 22:56:09 +0300

    Movie Website

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $('.carousel').carousel();
+});
+
+function changeBg (bg, title) {
+    let banner = document.querySelector(".banner");
+    let contents = document.querySelectorAll(".content");
+    banner.style.background = `url("../assets/movies/${bg}")`;
+    banner.style.backgroundSize = 'cover';
+    banner.style.backgroundPosition = 'cover';
+
+    contents.forEach(e => {
+        e.classList.remove("active");
+        if (e.classList.contains(title)) {
+            e.classList.add("active");
+        }
+    });
+}
+
+// document.querySelector(".carousel-item").onclick = changeBg;
+
+function toggleVideo () {
+    let trailer = document.querySelector(".trailer");
+    let video = document.querySelector("video");
+    video.pause();
+    trailer.classList.toggle("active");
+}
+
+document.querySelector(".close").onclick = toggleVideo;
+document.querySelector(".play").onclick = toggleVideo;
